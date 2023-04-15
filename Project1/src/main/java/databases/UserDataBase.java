b65047e253185b8deab73b96bbbe0b2f489867e1
Stefan Adamovic <78232915+StefanAdamovic@users.noreply.github.com>
2023-04-15 22:11:26 +0200

    Test2

--- Project1/src/main/java/databases/UserDataBase.java
+++ Project1/src/main/java/databases/UserDataBase.java
@@ -10,36 +10,30 @@ import model.User;
 public class UserDataBase {
 
 	// Simulacija baze korisnika
-	private static ArrayList<User> users = new ArrayList<User>(Arrays.asList(
-			AdminUser.getInstance(),
+	private static ArrayList<User> users = new ArrayList<User>(Arrays.asList(AdminUser.getInstance(),
 			new Architect("marko.gavrilovic@gmail.com", "123", "markogavrilovic"),
-			new Architect("stef.adamovic@gmail.com", "321", "stefanadamovic")
-		));
-	
-	
+			new Architect("stef.adamovic@gmail.com", "321", "stefanadamovic")));
+
 	// Provera da li je korisnik registrovan
-	// ne razumem zasto ne mogu da proverim ovom metodom uslov u servletu pa sam morao da koristim stream() umesto ovoga
-	public static boolean isRegistered(String username, String password) {  
+	// ne razumem zasto ne mogu da proverim ovom metodom uslov u servletu pa sam
+	// morao da koristim stream() umesto ovoga
+	public static boolean isRegistered(String username, String password) {
 		for (User user : users) {
-			if(user.getUserName().equals(username) && user.getPassword().equals(password) ) {
+			if (user.getUserName().equals(username) && user.getPassword().equals(password)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	
+
 	// Dodavanje korisnika u bazu korisnika
 	public static void addUser(String email, String password, String username) {
 		users.add(new User(email, password, username));
 	}
-	
-	
+
 	// Dohvatanje baze korisnika
 	public static ArrayList<User> getUsers() {
 		return users;
 	}
-
-	
 
 }
